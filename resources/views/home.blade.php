35bc97419789bbb9d74ba7f483268d055944bc78
samirazazy <samiralazazy@gmail.com>
2020-09-04 18:32:28 +0200

    Generating login flow with make:auth

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,14 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="row">
+        <div class=" col-3 p-5">
+            <img class="rounded-circle" src="https://scontent-hbe1-1.cdninstagram.com/v/t51.2885-19/s320x320/109249748_614803385833840_8617713047499411922_n.jpg?_nc_ht=scontent-hbe1-1.cdninstagram.com&_nc_ohc=HaNFmu8JaZAAX-tENwR&oh=60fa305cfbbd86a25b7a7a6881abb612&oe=5F7AE764" alt="avatar">
+        </div>
+        <div class="col-9">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
         </div>
     </div>
+
 </div>
 @endsection
